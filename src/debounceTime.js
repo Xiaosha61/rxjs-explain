07ec3e3a639f907a347414240bc8e23ab7d61be5
Xiaosha Xing <xiaosha.xing@logmein.com>
2022-05-18 17:12:31 +0200

    debounceTime: debounced events will NOT be handled, if the observable was unsubscribed

--- src/debounceTime.js
+++ src/debounceTime.js
@@ -1,27 +1,49 @@
 const Rx = require('rxjs');
+const programStartTime = Date.now();
 
 function handleEvent(event) {
-	console.log('===', event);
+	console.log('Event handled', event, Date.now() - programStartTime);
 	return event.index;
 }
 
 const newObserver = new Rx.Observable((subscriber) => {
 	// 1-3s
 	subscriber.next({ description: 'from constructor', time: 0 });
-	setTimeout(()=>subscriber.next({ description: 'from constructor', time: 0.9 }), 900);
-	setTimeout(()=>subscriber.next({ description: 'from constructor', time: 2.5 }), 2500);
+	setTimeout(()=>{
+		const eventSeq = 900;
+		console.log(`Event ${eventSeq} happens:`, Date.now() - programStartTime);
+		subscriber.next({ eventDescription: eventSeq });
+	}, 900);
+	setTimeout(()=>{
+		const eventSeq = 2500;
+		console.log(`Event ${eventSeq} happens:`, Date.now() - programStartTime);
+		subscriber.next({ eventDescription: eventSeq });
+	}, 2500);
 
 	// 4-6s nothing
 
 	// 7-9s
-	setTimeout(()=>subscriber.next({ description: 'from constructor', time: 7.7 }), 7700);
-	setTimeout(()=>subscriber.next({ description: 'from constructor', time: 8.8 }), 8800);
+	setTimeout(()=>{
+		const eventSeq = 7700;
+		console.log(`Event ${eventSeq} happens:`, Date.now() - programStartTime);
+		subscriber.next({ eventDescription: eventSeq });
+	}, 7700);
+	setTimeout(()=>{
+		const eventSeq = 8800;
+		console.log(`Event ${eventSeq} happens:`, Date.now() - programStartTime);
+		subscriber.next({ eventDescription: eventSeq });
+	}, 8800);
 });
 
-newObserver.pipe(Rx.filter(evt => true), Rx.debounceTime(3000)).subscribe((val) => handleEvent(val));
+const subscription = newObserver.pipe(Rx.filter(evt => true), Rx.debounceTime(3000)).subscribe((val) => handleEvent(val));
+setTimeout(() => {
+	subscription.unsubscribe();
+}, 9000); // if not unsubscribe, the debounced Event 8800 would have happened.
 
 /**
  * note: every event will get debounced!
  * 
  * debounceTime: on each new event, the previous one will be immediately dismissed, and the new one is debounced with n ms.
+ * 
+ * if unsubscribed after the event happened, the debounced events that could have been handled will NOT be handled anymore.
  */
